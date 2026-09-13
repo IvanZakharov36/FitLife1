1261edd878bda5f9ad7b30cdb5fe7184a087ddd6
Иван Захаров <munual23@yandex.ru>
2026-09-13 12:41:22 +0300

    изменения в подмодуле

--- fit_life.py
+++ fit_life.py
@@ -5,17 +5,22 @@ print('Добро пожаловать')
 # спрашиваем у пользователя имя и возраст
 user_name = input('Как вас зовут?')
 user_age = int(input('Сколько вам лет?'))
+
 # запрашиваем вес (в кг) и рост (в метрах)
 user_weight = float(input('Какой у вас вес? (в кг)'))
-user_height = float(input('Какой у вас рост? (например 1.50)'))
+user_height = float(input('Какой у вас рост?: '
+                          'например, 1.50'))
+
 # рассчитываем индекс массы тела
 bmi = round(user_weight / (user_height ** 2), 1)
+
 # рассчитываем норму воды в миллилитрах
 water_ml = float(user_weight * 30)
 water_l = float(water_ml / 1000)
-print()
-print(f'отчет для пользователя: {user_name} {user_age}')
+
+print(f'отчет для пользователя: {user_name} {user_age}\n')
 print(f'Ваш индекс массы тела: {bmi}')
-print(f'Рекомендуемая норма воды: {water_l} {'литров в день'}')
-print()
+print(f'Рекомендуемая норма воды: {water_l}'
+      '{литров в день}')
+
 print('Расчет окончен, будьте здоровы')
